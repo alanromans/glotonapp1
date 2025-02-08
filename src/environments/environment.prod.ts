7fb4c186bcecbbe1901d3b547a08479ccbdc39f3
alanromans <ala.romans@duocuc.cl>
2025-02-08 19:01:59 -0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDdiOngjE5R3wtKRjscZVPR0N5BgH9pIz0",
+    authDomain: "glotonapp-eac10.firebaseapp.com",
+    projectId: "glotonapp-eac10",
+    storageBucket: "glotonapp-eac10.firebasestorage.app",
+    messagingSenderId: "40606841590",
+    appId: "1:40606841590:web:ac8e8108ad11b663210ad7",
+    measurementId: "G-TWEVD7E3M5"
+  }
 };
