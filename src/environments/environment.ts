3769760ac5335162fea9b419eb30867f342bab0e
alanromans <ala.romans@duocuc.cl>
2025-02-08 19:01:28 -0300

    integrando firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDdiOngjE5R3wtKRjscZVPR0N5BgH9pIz0",
+    authDomain: "glotonapp-eac10.firebaseapp.com",
+    projectId: "glotonapp-eac10",
+    storageBucket: "glotonapp-eac10.firebasestorage.app",
+    messagingSenderId: "40606841590",
+    appId: "1:40606841590:web:ac8e8108ad11b663210ad7",
+    measurementId: "G-TWEVD7E3M5"
+  }
 };
 
 /*
